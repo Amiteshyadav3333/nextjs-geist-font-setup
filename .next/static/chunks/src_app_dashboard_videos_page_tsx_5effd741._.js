282255d871b4f6295bcf07cb948d8ea8c9749874
Amiteshyadav3333 <152839061+Amiteshyadav3333@users.noreply.github.com>
2025-07-26 17:01:43 +0530

    Add .next/static/chunks/src_app_dashboard_videos_page_tsx_5effd741._.js 1753529502897

---
+++ .next/static/chunks/src_app_dashboard_videos_page_tsx_5effd741._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_dashboard_videos_page_tsx_a18a6866._.js"
+],
+    source: "dynamic"
+});
